40e6e02bac8ba0e99728cf96223f961faba1069c
henewi <hpdunn2002@gmail.com>
2022-11-14 20:54:03 +0000

    //out code

--- javascript.js
+++ javascript.js
@@ -1,16 +1,16 @@
-const board = document.querySelector('.board');
-for(let rows = 0; rows < 8; rows++) {
-    let boxColour;
-    let boxStartColourWhite = rows % 2 === 0? true:false;
-    for(let columns = 0; columns < 8; columns++) {
-        if(boxStartColourWhite)
-            boxColour = columns % 2 === 0? 'white' : 'black';
-        else 
-            boxColour = columns % 2 === 0? 'black' : 'white';
-        const box = document.createElement('div');
-        box.style.width = board.clientWidth / 8 + 'px';
-        box.style.height = board.clientHeight / 8 + 'px';
-        box.style.backgroundColor = boxColour;
-        board.append(box);
-    }
-}
+// const board = document.querySelector('.board');
+// for(let rows = 0; rows < 8; rows++) {
+//     let boxColour;
+//     let boxStartColourWhite = rows % 2 === 0? true:false;
+//     for(let columns = 0; columns < 8; columns++) {
+//         if(boxStartColourWhite)
+//             boxColour = columns % 2 === 0? 'white' : 'black';
+//         else 
+//             boxColour = columns % 2 === 0? 'black' : 'white';
+//         const box = document.createElement('div');
+//         box.style.width = board.clientWidth / 8 + 'px';
+//         box.style.height = board.clientHeight / 8 + 'px';
+//         box.style.backgroundColor = boxColour;
+//         board.append(box);
+//     }
+// }
